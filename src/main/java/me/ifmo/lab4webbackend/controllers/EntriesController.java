f93a61c780d1e20764e83063624091851ae5d4c9
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-05-07 18:22:24 +0300

    add getUserEntries method

--- src/main/java/me/ifmo/lab4webbackend/controllers/EntriesController.java
+++ src/main/java/me/ifmo/lab4webbackend/controllers/EntriesController.java
@@ -6,4 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/entries")
 public class EntriesController {
+  @Autowired
+    private UserService userService;
+
+    @Autowired
+    private EntryRepository entryRepository;
+
+    @GetMapping
+    ResponseEntity<?> getUserEntries(Principal principal) {
+        User user = (User) this.userService.loadUserByUsername(principal.getName());
+        return ResponseEntity.ok(this.entryRepository.findByUser(user));
+    }
 }

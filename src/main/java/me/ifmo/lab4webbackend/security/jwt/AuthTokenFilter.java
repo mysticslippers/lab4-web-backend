2e5fb372277ab6654487af85fcdc58ac01461a51
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-05-07 17:49:24 +0300

    init AuthTokenFilter.java

---
+++ src/main/java/me/ifmo/lab4webbackend/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,26 @@
+package me.ifmo.lab4webbackend.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import me.ifmo.lab4webbackend.security.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class AuthTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtManager jwtManager;
+
+    @Autowired
+    private UserService userService;
+}

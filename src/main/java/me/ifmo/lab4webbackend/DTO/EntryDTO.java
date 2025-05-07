e17687b139f7ef98db480f29b1e1cc04783ff3b1
misticslippers <eternal.martyr13579@gmail.com>
2025-05-07 16:48:27 +0300

    add EntryDTO

---
+++ src/main/java/me/ifmo/lab4webbackend/DTO/EntryDTO.java
@@ -0,0 +1,12 @@
+package me.ifmo.lab4webbackend.DTO;
+
+import lombok.*;
+
+@Data
+public class EntryDTO {
+    private final double x;
+
+    private final double y;
+
+    private final double r;
+}

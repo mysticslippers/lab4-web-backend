b0ec9a33d6d648569bd0d7df0e725948875b81ab
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-05-07 17:50:47 +0300

    add parseJwt method

--- src/main/java/me/ifmo/lab4webbackend/security/jwt/AuthTokenFilter.java
+++ src/main/java/me/ifmo/lab4webbackend/security/jwt/AuthTokenFilter.java
@@ -24,6 +24,14 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     @Autowired
     private UserService userService;
 
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7);
+        }
+        return null;
+    }
+    
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {

464a5709450202a1fbcbacf05f7d5b0d9fddc486
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-05-07 18:19:40 +0300

    init EntriesController.java

---
+++ src/main/java/me/ifmo/lab4webbackend/controllers/EntriesController.java
@@ -0,0 +1,9 @@
+package me.ifmo.lab4webbackend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/entries")
+public class EntriesController {
+}
